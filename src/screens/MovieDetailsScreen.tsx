0ecce219b9792bcf5e064ad5ca9a985ce9b99167
Eric Brito <eric_brito@msn.com>
2026-04-23 21:16:24 -0300

    Tela de Listagem e de Detalhes

--- src/screens/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen.tsx
@@ -1,18 +1,109 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign'
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import PlayButton from "../components/PlayButton";
+import { Movie } from "../components/MovieRow";
+
+import { useRoute } from "@react-navigation/native";
 
 export default function MovieDetailsScreen() {
+    const route = useRoute()
+
+    const { movie } = route.params as { movie: Movie }
+
     return(
-        <View style={styles.container}>
-            <Text>MovieDetailsScreen</Text>
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+            {/* Imagem */}
+            <View>
+                <Image 
+                    style={styles.image}
+                    source={{uri: movie.poster}}
+                />
+                <LinearGradient
+                    style={styles.gradient}
+                    colors={['transparent', 'white']}
+                    start={{x: 0, y: 0.5}}
+                    end={{x: 0, y: 1}}
+                />
+            </View>
+
+            <View style={ styles.content}>
+            {/* Título */}
+                <Text style={styles.title}>{movie.title}</Text>
+
+                {/* Nota */}
+                <View style={styles.rating}>
+                    <AntDesign name="star" size={20} color="#f7cb46" />
+                    <Text style={[styles.text, {marginLeft: 12,}]}>{movie.rating}/10</Text>
+                </View>
+
+                <Text style={[styles.text, {marginBottom: 12}]}>
+                    {movie.categories}
+                </Text>
+
+                <PlayButton />
+
+                <ScrollView
+                    style={styles.synopsisContainer}
+                    contentContainerStyle={styles.synopsisContent}
+                >
+                    <Text style={[styles.text, {fontSize: 18, fontWeight: '600'}]}>
+                        Sinopse
+                    </Text>
+                    <Text style={[styles.text, {marginBottom: 24,}]}>
+                        {movie.synopsis}
+                    </Text>
+                </ScrollView>
+            </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
 
 const styles = StyleSheet.create({
+    synopsisContainer: {
+        flex: 1,
+        backgroundColor: '#e8e8e8',
+        padding: 14,
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 12,
+    },
+    text: {
+        fontSize: 17,
+    },
+    rating: {
+        flexDirection: 'row',
+        marginBottom: 12,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginVertical: 12,
+    },
+    gradient: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    content: {
+        paddingHorizontal: 20,
+        flex: 1
+    },
+    image: {
+        width: '100%',
+        height: 320,
+    },
     container: {
         flex: 1,
         backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
+    },
+    synopsisContent: {
+        flexGrow: 1,
+        justifyContent: 'center'
     }
 })
--- src/screens/MovieListScreen.tsx
+++ src/screens/MovieListScreen.tsx
@@ -1,26 +1,73 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native"
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+import MovieRow from "../components/MovieRow";
+
+import movies from "../data/movies.json"
 
 export default function MovieListScreen() {
-
     const navigation = useNavigation()
 
     return(
-        <View style={styles.container}>
-            <Text>MovieListScreen</Text>
-            <Button
-                title="Ir para tela de detalhes"
-                onPress={() => navigation.navigate("MovieDetailsScreen")}
-            />
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Filmes</Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('MovieFormScreen')}
+                    >
+                        <Text style={styles.formButton}>
+                            +
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <FlatList
+                    style={styles.list}
+                    data={movies}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('MovieDetailsScreen', { movie: item })}
+                        >
+                            <MovieRow movie={item}/>
+                        </TouchableOpacity>
+                    )}
+                    ItemSeparatorComponent={() => <View style={styles.separator}></View>}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
 
 const styles = StyleSheet.create({
+    formButton: {
+        color: '#eb4435',
+        fontSize: 32,
+        fontWeight: 'black'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
+        backgroundColor: "#f2f2f2",
+        paddingTop: 20,
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold'
+    },
+    list: {
+        marginVertical: 16,
+        paddingHorizontal: 8,
+        backgroundColor: '#fff',
+        borderRadius: 12,
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#eee'
+    },
 })
